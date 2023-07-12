e007244407cf7751eab3bf567184785699ef998c
Ross Buchan <rossbuchan1@googlemail.com>
2023-07-12 13:55:41 +0100

    Create sell drink method in coffee_shop and test (finihed MVP)

--- coffee_shop_start/src/coffee_shop.py
+++ coffee_shop_start/src/coffee_shop.py
@@ -6,3 +6,6 @@ class CoffeeShop:
 
 	def change_till_by_amount(self, amount):
 		self.till += amount
+
+	def sell_drink(self, drink):
+		self.change_till_by_amount(drink.price)
--- coffee_shop_start/tests/coffee_shop_test.py
+++ coffee_shop_start/tests/coffee_shop_test.py
@@ -1,10 +1,12 @@
 import unittest
 from src.coffee_shop import CoffeeShop
+from src.drink import Drink
 
 class TestCoffeeShop(unittest.TestCase):
 
     def setUp(self):
         self.coffee_shop = CoffeeShop("The Prancing Pony", 100)
+        self.drink = Drink("espresso", 2)
     
     def test_coffee_shop_has_name(self):
         self.assertEqual("The Prancing Pony", self.coffee_shop.name)
@@ -19,3 +21,7 @@ class TestCoffeeShop(unittest.TestCase):
     def test_decrease_till(self):
         self.coffee_shop.change_till_by_amount(-10)
         self.assertEqual(90, self.coffee_shop.till)
+
+    def test_can_sell_drink(self):
+        self.coffee_shop.change_till_by_amount(self.drink.price)
+        self.assertEqual(102, self.coffee_shop.till)
